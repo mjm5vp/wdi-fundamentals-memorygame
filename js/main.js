7f3e1c08ed81c3bd8328c1956906e3af27008f8a
Mark Moeller <MarkMoellerUVA@gmail.com>
2017-04-23 23:50:36 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,20 +1,26 @@
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2){
-	if (cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
-	}else{
-		alert("Sorry try again :(");
+var checkForMatch = function(){
+	if (cardsInPlay.length === 2){
+		if (cardsInPlay[0] === cardsInPlay[1]){
+			alert("You found a match!");
+		}else{
+			alert("Sorry try again :(");
+		}
 	}
 }
 
+var flipCard = function(cardID){
+
+	console.log("User flipped " + cards[cardID]);
+
+	cardsInPlay.push(cards[cardID]);
+
+	checkForMatch()
+}
+
+flipCard(0);
+flipCard(2);
 
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
