6c6c97df2b5b2e046aa89d4b5c9ab5d957d9e081
Mark Moeller <MarkMoellerUVA@gmail.com>
2017-04-25 16:13:24 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen","queen","king","king"];
+var cards = [
+{
+	rank:"queen",
+	suit:"hearts",
+	cardImage:"images/queen-of-hearts.png"
+},
+{
+	rank:"queen",
+	suit:"diamonds",
+	cardImage:"images/queen-of-diamonds.png"
+},
+{
+	rank:"king",
+	suit:"hearts",
+	cardImage:"images/king-of-hearts.png"
+},
+{
+	rank:"king",
+	suit:"diamonds",
+	cardImage:"images/king-of-diamonds.png"
+}
+]
 var cardsInPlay = [];
 
 var checkForMatch = function(){
@@ -13,11 +34,14 @@ var checkForMatch = function(){
 
 var flipCard = function(cardID){
 
-	console.log("User flipped " + cards[cardID]);
+	console.log("User flipped " + cards[cardID].rank);
 
-	cardsInPlay.push(cards[cardID]);
+	cardsInPlay.push(cards[cardID].rank);
 
-	checkForMatch()
+	checkForMatch();
+
+	console.log(cards[cardID].suit);
+	console.log(cards[cardID].cardImage);
 }
 
 flipCard(0);
